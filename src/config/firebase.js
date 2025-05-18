a8c6a99bcf69d579e63755036cff8d29c4aea2d0
Tanisha <vermatanisha620@gmail.com>
2025-05-18 16:33:47 +0530

    Implement logout logic

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 
@@ -50,4 +50,13 @@ const login = async (email,password) => {
        }
 }
 
-export {signup,login}
+const logout = async  () => {
+     try {
+         await signOut(auth)  
+     } catch (error) {
+         console.error(error);
+         toast.error(error.code.split('/')[1].split('-').join(" "));
+     }
+}
+
+export {signup,login,logout}
