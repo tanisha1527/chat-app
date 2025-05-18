ede48d5128887cd8f6d286dfa8a3b1ba13802f05
Tanisha <vermatanisha620@gmail.com>
2025-05-18 16:19:42 +0530

    Add sign-up functionality

---
+++ src/config/firebase.js
@@ -0,0 +1,44 @@
+
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABqkeU_FhgrNH6QudgVLfrdsMwtI0QrEQ",
+  authDomain: "chat-app-dd079.firebaseapp.com",
+  projectId: "chat-app-dd079",
+  storageBucket: "chat-app-dd079.firebasestorage.app",
+  messagingSenderId: "587589542582",
+  appId: "1:587589542582:web:deeff0c164f3ca95b7fc17"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async(username,email,password) => {
+     try {
+          const res = await createUserWithEmailAndPassword(auth,email,password);
+          const user = res.user;
+          await setDoc(doc(db,"users",user.uid),{
+               id:user.uid,
+               username:username.toLowerCase(),
+               email,
+               name:"",
+               avatar:"",
+               bio:"Hey, There i am using chat app",
+               lastSeen:Date.now()
+          })
+          await setDoc(doc(db,"chats",user.uid),{
+              chatData:[]
+          })
+     } catch (error) {
+         console.error(error)
+         toast.error(error.code)
+      
+     }
+}
+
+export {signup}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,31 @@
 import './Login.css'
 import assets from '../../assets/assets'
 import { useState } from 'react'
+import { signup } from '../../config/firebase'
+
 
 const Login = () => {
 
   const [currentState,setCurrentState] = useState("Sign up");
+  const [userName,setUserName] = useState("");
+  const [email,SetEmail] = useState("");
+  const [password,setPasword] = useState("");
+  
+  const onSubmitHandler = (event) => {
+      event.preventDefault();
+      if(currentState=== "Sign up") {
+         signup(userName,email,password);
+      }
+  }
 
   return (
     <div className='login'>
        <img src={assets.logo_big} alt='' className='logo' />
-       <form className='login-form'>
+       <form onSubmit={onSubmitHandler} className='login-form'>
           <h2>{currentState}</h2>
-          {currentState === "Sign up"? <input type='text' placeholder='username' className='form-input' required />:null}
-          <input type='email' placeholder='Email address' className='form-input' required />
-          <input type='password' placeholder='password' className='form-input' required />
+          {currentState === "Sign up"? <input onChange={(e)=>setUserName(e.target.value)} value={userName} type='text' placeholder='username' className='form-input' required />:null}
+          <input onChange={(e)=>SetEmail(e.target.value)} value={email} type='email' placeholder='Email address' className='form-input' required />
+          <input onChange={(e)=>setPasword(e.target.value)} value={password} type='password' placeholder='password' className='form-input' required />
           <button type='submit'>{currentState === "Sign up"?"Create account":"Login now"}</button>
           <div className="login-term">
              <input type='checkbox' />
