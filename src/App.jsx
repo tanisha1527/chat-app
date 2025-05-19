19fc9bf6c6fb06e508d61b460bf5b9bca1f25188
Tanisha <vermatanisha620@gmail.com>
2025-05-19 17:33:46 +0530

    add profile update feature with image upload to Firebase Storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import { Route, Routes, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Chat from './pages/Chat/Chat'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
-import { ToastContainer, toast } from 'react-toastify';
-import { useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { useContext, useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './config/firebase';
+import { AppContext } from './context/AppContext';
   
 
 const App = () => {
 
   const navigate = useNavigate();
+  const {loadUserData} = useContext(AppContext);
 
 
   useEffect(()=> {
       onAuthStateChanged(auth, async (user) => {
           if (user) {
             navigate('/chat')
+            await loadUserData(user.uid)
+            
           }
           else{
             navigate('/')
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,23 +1,85 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import assets from '../../assets/assets'
 import './ProfileUpdate.css'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth, db } from '../../config/firebase';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import upload from '../../lib/upload';
 
 const ProfileUpdate = () => {
 
+  const navigate = useNavigate();
   const [image,setImage] = useState(false);
+  const [name,setName] = useState("");
+  const [bio,setBio] = useState("");
+  const [uid,setUid] = useState("");
+  const [prevImage,setPrevImage] = useState("");
+
+  const profileUpdate = async (event) => {
+      event.preventDefault();
+      try {
+
+        if (!prevImage && !image) {
+            toast.error("upload profile picture")
+        }
+        const docRef = doc(db,'users',uid);
+        if (image) {
+          const imgUrl = await upload(image);
+           setPrevImage(imgUrl);
+           await updateDoc(docRef,{
+            avatar:imgUrl,
+            bio:bio,
+            name:name
+           })
+        }
+        else{
+          await updateDoc(docRef,{
+            bio:bio,
+            name:name
+           })
+        }
+      } catch (error) {
+        
+      }
+  } 
+
+  useEffect(()=>{
+       onAuthStateChanged(auth,async (user)=> {
+         if (user) {
+           setUid(user.uid)
+           const docRef = doc(db,"users",user.uid);
+           const docSnap = await getDoc(docRef);
+           if (docSnap.data().name) {
+              setName(docSnap.data().name);
+           }
+           if (docSnap.data().bio) {
+              setBio(docSnap.data().bio);
+           }
+           if (docSnap.data().avatar) {
+              setPrevImage(docSnap.data().avatar);
+           }
+         }
+         else{
+            navigate('/')
+         }
+       })
+  },[])
+  
 
   return (
     <div className='profile'>
         <div className="profile-container">
-          <form>
+          <form onSubmit={profileUpdate}>
               <h3>Profile Details</h3>
               <label htmlFor="avatar">
                 <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
                 <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
                 upload profile image
               </label>
-              <input type="text" placeholder='Your name' required />
-               <textarea placeholder='Write profile bio' required></textarea>
+              <input onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Your name' required />
+               <textarea onChange={(e)=>setBio(e.target.bio)} value={bio} placeholder='Write profile bio' required></textarea>
                <button type='submit'>Save</button>
           </form>
           <img src={image? URL.createObjectURL(image) : assets.logo_icon} className='profile-photo' alt="" />
