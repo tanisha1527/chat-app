65cb65ba9cf2a8d658f07a365ee84333ddf8a0f3
Tanisha <vermatanisha620@gmail.com>
2025-05-22 20:58:33 +0530

    Mark message as seen on chat select and prevent duplicate user set

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,7 +1,7 @@
 import './LeftSidebar.css'
 import assets from '../../assets/assets'
 import { useNavigate } from 'react-router-dom'
-import { arrayUnion, collection, doc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore';
+import { arrayUnion, collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore';
 import { db } from '../../config/firebase';
 import { useContext, useState } from 'react';
 import { AppContext } from '../../context/AppContext';
@@ -10,7 +10,7 @@ import { toast } from 'react-toastify';
 const LeftSidebar = () => {
 
   const navigate = useNavigate();
-  const { userData,chatData,chatUser,setChatUser,setMessagesId,messagesId } = useContext(AppContext);
+  const { userData, chatData, chatUser, setChatUser, setMessagesId, messagesId } = useContext(AppContext);
   const [user, setUser] = useState(null);
   const [showSearch, setShowSearch] = useState(false);
 
@@ -23,15 +23,15 @@ const LeftSidebar = () => {
         const q = query(userRef, where("username", "==", input.toLowerCase()));
         const querySnap = await getDocs(q);
         if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
-            let userExist = false
-            chatData.map((user)=> {
-               if (user.rId === querySnap.docs[0].data().id) {
-                 userExist = true;
-               }
-            })
-            if (!userExist) {
-              setUser(querySnap.docs[0].data()) 
+          let userExist = false
+          chatData.map((user) => {
+            if (user.rId === querySnap.docs[0].data().id) {
+              userExist = true;
             }
+          })
+          if (!userExist) {
+            setUser(querySnap.docs[0].data())
+          }
         }
         else {
           setUser(null);
@@ -82,8 +82,20 @@ const LeftSidebar = () => {
   }
 
   const setChat = async (item) => {
+    try {
       setMessagesId(item.messageId);
       setChatUser(item)
+      const userChatsRef = doc(db, 'chats', userData.id);
+      const userChatsSnapshot = await getDoc(userChatsRef);
+      const userChatsData = userChatsSnapshot.data();
+      const chatIndex = userChatsData.chatsData.findIndex((c) => c.messageId === item.messageId);
+      userChatsData.chatsData[chatIndex].messageSeen = true;
+      await updateDoc(userChatsRef, {
+        chatsData: userChatsData.chatsData
+      })
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   return (
@@ -108,15 +120,15 @@ const LeftSidebar = () => {
       <div className="ls-list">
         {showSearch && user
           ? <div onClick={addChat} className='friends add-user'>
-             <img src={user.avatar} alt="" />
+            <img src={user.avatar} alt="" />
             <p>{user.name}</p>
           </div>
           : chatData.map((item, index) => (
-            <div onClick={()=>setChat(item)} key={index} className="friends">
+            <div onClick={() => setChat(item)} key={index} className={`friends ${item.messageSeen || item.messageId === messagesId ? "" : "border"}`}>
               <img src={item.userData.avatar} alt="" />
               <div>
-                 <p>{item.userData.name}</p>
-                 <span>{item.lastMessage}</span>
+                <p>{item.userData.name}</p>
+                <span>{item.lastMessage}</span>
               </div>
             </div>
           ))
